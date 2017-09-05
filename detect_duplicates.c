67d87d014abe399cd1fecef05f9c1ace4b382dfb
Maxence Ahlouche <maxence.ahlouche@gmail.com>
2017-09-05 18:12:33 +0200

    Deinitialize the csv parser when done with it

--- detect_duplicates.c
+++ detect_duplicates.c
@@ -6,10 +6,14 @@ const char DATASET_FILE[] = "restaurant_dataset.csv";
 
 int main() {
     csv_parser p;
+    // initialize the csv parser
     if (csv_init(&p, CSV_STRICT) != 0) {
         fprintf(stderr, "Failed to initialize csv parser\n");
         exit(EXIT_FAILURE);
     }
+
+    // deinit the csv parser
+    csv_fini(&p, NULL, NULL, NULL);
 
     return 0;
 }
