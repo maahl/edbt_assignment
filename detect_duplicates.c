2a8b5cba3744e65fb807125a9f839518a8bfafa3
Maxence Ahlouche <maxence.ahlouche@gmail.com>
2017-09-05 18:04:07 +0200

    Add a csv parsing lib

--- detect_duplicates.c
+++ detect_duplicates.c
@@ -1,3 +1,15 @@
+#include "csv.h"
+
+typedef struct csv_parser csv_parser;
+
+const char DATASET_FILE[] = "restaurant_dataset.csv";
+
 int main() {
+    csv_parser p;
+    if (csv_init(&p, CSV_STRICT) != 0) {
+        fprintf(stderr, "Failed to initialize csv parser\n");
+        exit(EXIT_FAILURE);
+    }
+
     return 0;
 }
